64f26d5c494e25c23421568f09a88f688e89b934
cpoakley1432 <cpoakley1432@gmail.com>
2015-12-10 09:24:27 -0500

    update from my computer

--- src/main/java/com/pams/services/ItemRepository.java
+++ src/main/java/com/pams/services/ItemRepository.java
@@ -1,7 +1,10 @@
 package com.pams.services;
 
+import com.sun.tools.javac.jvm.Items;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by MattBrown on 12/9/15.
  */
-public interface ItemRepository {
+public interface ItemRepository extends CrudRepository<Items, Integer> {
 }
