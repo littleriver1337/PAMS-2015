87fff6a72e7c855b5de519f8018b9baba4cba3a8
Alison Arsenault <AlisonHorta@Alisons-MacBook-Pro.local>
2015-12-10 13:12:55 -0500

    Adjusted login button

--- public/logIn/login.controller.js
+++ public/logIn/login.controller.js
@@ -5,8 +5,9 @@ angular
   .module('pamsLogin')
   .controller('LoginController', function ($scope, LoginService, $routeParams) {
     var vm = this;
-
+    console.log("INSTATIATED");
     vm.login = function(userInfo){
+      console.log("SUBMITTED", userInfo);
       LoginService.login(userInfo);
     };
 
