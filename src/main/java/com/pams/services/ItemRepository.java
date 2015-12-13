9bc60e8a11b7dc9e10e38bd9d343be8028789bb0
MattBrown <litteriver1337@gmail.com>
2015-12-13 10:45:24 -0500

    refactored the items repository to Club repository added csv file and upload of csv file and @PostConstruct implemented subsequent tests for addUser and editUser

--- src/main/java/com/pams/services/ItemRepository.java
+++ src/main/java/com/pams/services/ItemRepository.java
@@ -1,10 +1,10 @@
 package com.pams.services;
 
-import com.pams.entities.Item;
+import com.pams.entities.Clubs;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by MattBrown on 12/9/15.
  */
-public interface ItemRepository extends CrudRepository<Item, Integer> {
+public interface ItemRepository extends CrudRepository<Clubs, Integer> {
 }
