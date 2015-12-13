19018a24544ce6af68790811698129ca06ab356a
MattBrown <litteriver1337@gmail.com>
2015-12-13 12:26:23 -0500

    added new jar

--- src/test/java/com/pams/Pams2015ApplicationTests.java
+++ src/test/java/com/pams/Pams2015ApplicationTests.java
@@ -17,8 +17,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertTrue;
-
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Pams2015Application.class)
 @WebAppConfiguration
 public class Pams2015ApplicationTests {
